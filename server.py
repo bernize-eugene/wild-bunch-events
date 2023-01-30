23e9e4ee37cfbd4074f2f78432bc79d710a61317
Bernize Eugene <bernize.eugene123@gmail.com>
2023-01-30 12:57:26 -0400

    Test homepage -- ok!

--- server.py
+++ server.py
@@ -1,22 +1,22 @@
 from model import connect_to_db, db
 import os
 from jinja2 import StrictUndefined
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
 app.secret_key = "SECRETSECRETSECRETSCETRE"
 app.jinja_env.undefined = StrictUndefined
 
-GM_KEY = os.environ["GOOGLE_MAPS_KEY"]
-CLOUD_KEY = os.environ["CLOUDINARY_KEY"]
-CLOUD_SECRET = os.environ["CLOUDINARY_SECRET"]
-CLOUD_NAME = os.environ["CLOUD_NAME"]
+GM_KEY = os.environ['GOOGLE_MAPS_KEY']
+CLOUD_KEY = os.environ['CLOUDINARY_KEY']
+CLOUD_SECRET = os.environ['CLOUDINARY_SECRET']
+CLOUD_NAME = os.environ['CLOUD_NAME']
 
 
 @app.route('/', methods=['GET'])
 def display_homepage():
-    return jsonify({"Hello": "World"})
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.debug = True
