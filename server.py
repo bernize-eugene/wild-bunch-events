b4a062fd18acc776e7daa908ef4e3ccc7cd2fa9a
= <bernize.eugene123@gmail.com>
2023-03-06 22:59:05 -0400

    add register route to process sign up, create register.html -- test ok

--- server.py
+++ server.py
@@ -4,6 +4,9 @@ import os
 from jinja2 import StrictUndefined
 from flask import Flask, jsonify, render_template, request, redirect, flash, session
 from flask_cors import CORS
+import cloudinary.uploader
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 app.secret_key = "SECRETSECRETSECRETSCETRE"
@@ -14,6 +17,9 @@ CLOUD_KEY = os.environ['CLOUDINARY_KEY']
 CLOUD_SECRET = os.environ['CLOUDINARY_SECRET']
 CLOUD_NAME = os.environ['CLOUD_NAME']
 
+st_lucia_tz = pytz.timezone('America/St_Lucia')
+current_time = datetime.now(st_lucia_tz)
+timestamp = current_time.isoformat()
 
 @app.route('/', methods=['GET'])
 def displayHomepage():
@@ -52,6 +58,63 @@ def logout():
 
     session.pop('username', None)
     return redirect('/')
+
+# Register
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    msg = ""
+
+    # Is all required information available?
+    if request.method == "POST":
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        profileFile = request.files['profileUrl']
+        email = request.form['email']
+        phone = request.form['phone']
+        username = request.form['username']
+        password_hash = request.form['password']
+        city = request.form['userCity']
+        region = request.form['userRegion']
+        country = request.form['userCountry']
+
+        # Does the user have an account?
+        user = crud.get_user_by_username(username)
+
+        result = cloudinary.uploader.upload(profileFile, 
+        api_key=CLOUD_KEY, 
+        api_secret=CLOUD_SECRET, 
+        cloud_name=CLOUD_NAME, timestamp=timestamp)
+
+        profileUrl = result['secure_url']
+
+        if user: 
+            msg = 'Account already exists.'
+            flash(f'{msg}')
+            return redirect('/login')
+
+        else: 
+            newUser = crud.create_user(
+                firstName,
+                lastName,
+                email, 
+                phone, 
+                username,
+                profileUrl,
+                password_hash,
+                city, 
+                region,
+                country)
+            db.session.add(newUser)
+            db.session.commit()
+            db.session.close()
+
+            # where does the user go after creating an account?
+            user = crud.get_user_by_username(username)
+            userId = user.userId
+
+            return redirect(f'/users/{userId}')
+    return render_template("register.html", msg=msg)
+
 
 # User Dashboard
 @app.route("/users/<userId>")
